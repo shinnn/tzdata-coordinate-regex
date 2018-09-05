3e76451f60714d0734f4cdb7c3213c06460248b9
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-09-05 10:40:58 +0900

    update devDependencies
    Highlights:
    
    * update ESLint form v4 to v5 https://eslint.org/docs/user-guide/migrating-to-5.0.0
    * introduce rollup-config-module to adapt to .mjs-based ecosystem
    * update @shinnn/eslint-config and follow its rules, for exmaple tab indent
    * introduce regexp-tree to check if regular expressions are fully optimized

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-'use strict';
-
-var module$1 = /^([+-])([0-8]\d|90)([0-5]\d)(?<latitudeSeconds>[0-5]\d(?=[+-]\d{7}))?([+-])(0\d{2}|1(?:[0-7]\d|80))([0-5]\d)(?<longitudeSeconds>(?<=[+-]\d{6}[+-]\d{5})[0-5]\d)?$/u;
-
-module.exports = module$1;
+module.exports = /^([+-])([0-8]\d|90)([0-5]\d)(?<latitudeSeconds>[0-5]\d(?=.{8}))?([+-])(0\d{2}|1(?:[0-7]\d|80))([0-5]\d)(?<longitudeSeconds>(?<=.{13})[0-5]\d)?$/u;
--- test.js
+++ test.js
@@ -1,97 +1,103 @@
 'use strict';
 
+const {optimize} = require('regexp-tree');
 const test = require('tape');
 const tzdataCoordinateRegex = require('.');
 
 test('tzdataCoordinateRegex', t => {
-  t.deepEqual(
-    [...tzdataCoordinateRegex.exec('+4011+04430')],
-    [
-      '+4011+04430',
-      '+',
-      '40',
-      '11',
-      undefined,
-      '+',
-      '044',
-      '30',
-      undefined
-    ],
-    'should matche TZ-database-style time zone coordinates.'
-  );
+	t.deepEqual(
+		[...tzdataCoordinateRegex.exec('+4011+04430')],
+		[
+			'+4011+04430',
+			'+',
+			'40',
+			'11',
+			undefined,
+			'+',
+			'044',
+			'30',
+			undefined
+		],
+		'should matche TZ-database-style time zone coordinates.'
+	);
 
-  t.deepEqual(
-    [...tzdataCoordinateRegex.exec('-690022+0393524')],
-    [
-      '-690022+0393524',
-      '-',
-      '69',
-      '00',
-      '22',
-      '+',
-      '039',
-      '35',
-      '24'
-    ],
-    'should matche TZ-database-style time zone coordinates with seconds.'
-  );
+	t.deepEqual(
+		[...tzdataCoordinateRegex.exec('-690022+0393524')],
+		[
+			'-690022+0393524',
+			'-',
+			'69',
+			'00',
+			'22',
+			'+',
+			'039',
+			'35',
+			'24'
+		],
+		'should matche TZ-database-style time zone coordinates with seconds.'
+	);
 
-  const {groups} = tzdataCoordinateRegex.exec('-905958-1805959');
+	const {groups} = tzdataCoordinateRegex.exec('-905958-1805959');
 
-  t.equal(
-    groups.latitudeSeconds,
-    '58',
-    'should capture latitude seconds as a named group if available.'
-  );
+	t.equal(
+		groups.latitudeSeconds,
+		'58',
+		'should capture latitude seconds as a named group if available.'
+	);
 
-  t.equal(
-    groups.longitudeSeconds,
-    '59',
-    'should capture longitude seconds as a named group if available.'
-  );
+	t.equal(
+		groups.longitudeSeconds,
+		'59',
+		'should capture longitude seconds as a named group if available.'
+	);
 
-  t.equal(
-    tzdataCoordinateRegex.exec('-112233+1122'),
-    null,
-    'should not match invalid tz coordinates.'
-  );
+	t.equal(
+		tzdataCoordinateRegex.exec('-112233+1122'),
+		null,
+		'should not match invalid tz coordinates.'
+	);
 
-  t.equal(
-    tzdataCoordinateRegex.exec('-9100+00000'),
-    null,
-    'should take latitude range (-90 ~ +90) into consideration.'
-  );
+	t.equal(
+		tzdataCoordinateRegex.exec('-9100+00000'),
+		null,
+		'should take latitude range (-90 ~ +90) into consideration.'
+	);
 
-  t.equal(
-    tzdataCoordinateRegex.exec('+000000-1810000'),
-    null,
-    'should take longitude range (-180 ~ +180) into consideration.'
-  );
+	t.equal(
+		tzdataCoordinateRegex.exec('+000000-1810000'),
+		null,
+		'should take longitude range (-180 ~ +180) into consideration.'
+	);
 
-  t.equal(
-    tzdataCoordinateRegex.exec('+006000-0000000'),
-    null,
-    'should validate latitude minutes.'
-  );
+	t.equal(
+		tzdataCoordinateRegex.exec('+006000-0000000'),
+		null,
+		'should validate latitude minutes.'
+	);
 
-  t.equal(
-    tzdataCoordinateRegex.exec('+000000-0006000'),
-    null,
-    'should validate longitude minutes.'
-  );
+	t.equal(
+		tzdataCoordinateRegex.exec('+000000-0006000'),
+		null,
+		'should validate longitude minutes.'
+	);
 
-  t.equal(
-    tzdataCoordinateRegex.exec('+000060-0000000'),
-    null,
-    'should validate latitude seconds.'
-  );
+	t.equal(
+		tzdataCoordinateRegex.exec('+000060-0000000'),
+		null,
+		'should validate latitude seconds.'
+	);
 
-  t.equal(
-    tzdataCoordinateRegex.exec('+000000-0000060'),
-    null,
-    'should validate longitude seconds.'
-  );
+	t.equal(
+		tzdataCoordinateRegex.exec('+000000-0000060'),
+		null,
+		'should validate longitude seconds.'
+	);
 
-  t.end();
+	t.equal(
+		optimize(tzdataCoordinateRegex).toRegExp().source,
+		tzdataCoordinateRegex.source,
+		'should be fully optimized.'
+	);
+
+	t.end();
 });
-
